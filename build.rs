2df2943850a1a482b22e9b853ceef3d3b2f311c1
Shupei Fan <dymarkfan@outlook.com>
2025-02-28 13:42:56 +0800

    Cleanup build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 use cmake;
 
 fn main() {
+    println!("cargo::rerun-if-changed=DRAMsim3");
+
     let dst = cmake::build("DRAMsim3");
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=dylib=stdc++");
-    println!("cargo:rustc-link-lib=static=dramsim3");
+    println!("cargo::rustc-link-search=native={}/lib", dst.display());
+    println!("cargo::rustc-link-lib=static=dramsim3");
+    println!("cargo::rustc-link-lib=dylib=stdc++");
 }
